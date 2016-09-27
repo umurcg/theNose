385425cdfd6bca71e774d312b4673e34ca3e1977
umr <umurcg@gmail.com>
2016-09-27 21:56:34 +0300

    Script added. Buildings become transparent when player gets back of building. Cat drawed and rigged. Cat animation added.

--- The Overcoat/Assets/DrawerScript.cs
+++ The Overcoat/Assets/DrawerScript.cs
@@ -3,33 +3,57 @@ using System.Collections;
 
 public class DrawerScript : MonoBehaviour, IClickAction {
 	GameObject player;
+	Animator acPlayer;
+	PlayerComponentController pcc;
+	float timer=0;
+	public float holdTime=5;
+	bool used=false;
+	ObjectChildSpawn ocs;
+	public GameObject door;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
+		acPlayer = player.GetComponent <Animator>();
+		pcc = player.GetComponent <PlayerComponentController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (timer > 0) {
+			timer -= Time.deltaTime;
+
+			if (timer <= 0) {
+				acPlayer.SetBool ("Hands",false);
+			
+				callSpawn ();
+				timer = 0;
+			}
+		}
 	
+	}
+	void callSpawn(){
+
+				ocs.Spawn ();
+		pcc.ContinueToWalk();
+	
+		door.GetComponent<ChangeMaterial> ().change();
+		door.GetComponent<KeySlideCompletely> ().enabled = true;
+		door.transform.tag = "ActiveObject";
+	    
 	}
 
 	public void Action(){
-
-		if (player != null) {
-
-			ObjectChildSpawn ocs = player.GetComponentInChildren<ObjectChildSpawn> ();
-			if(ocs!=null)
-			ocs.Spawn ();
-
-//			for (int i = 0; i < player.transform.childCount; i++) {
-//				Transform child = player.transform.GetChild (i);
-//				if (child.name == "Nose") {
-//					ObjectChildSpawn ocs = child.GetComponent<ObjectChildSpawn> ();
-//					ocs.Spawn ();
-//				}
-//			}
+		if (player != null&&used==false) {
+			used = true;
+			ocs = player.GetComponentInChildren<ObjectChildSpawn> ();
+			if (ocs != null) {
+				timer = 5;
+				acPlayer.SetBool ("Hands", true);
+				pcc.StopToWalk ();
+			}
 		}
-
 	}
+
+
 
 }
---
+++ The Overcoat/Assets/SeeBehindWall.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class SeeBehindWall : MonoBehaviour {
+	Material originalMat;
+	public Material transparent;
+	GameObject player;
+	bool isTransparent=false;
+	Renderer rend;
+	public float speed=0.01f;
+	public GameObject[] targetObjects;
+
+
+	Color color;
+	// Use this for initialization
+
+
+	void Start () {
+
+
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+
+		rend = GetComponent<Renderer> ();
+		originalMat = rend.material;
+		color.r = 1;
+		color.g = 1;
+		color.b = 1;
+	}
+
+	void makeTransparent(){
+		if (isTransparent == false) {
+			isTransparent = true;
+
+
+			color.a = 1;
+			transparent.color = color;
+			rend.material = transparent;
+
+
+		}
+
+
+	}
+
+	void makeSolid(){
+		if (isTransparent) {
+			isTransparent = false;
+
+
+
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		rayCast ();
+      
+		if (targetObjects.Length > 0)
+			rayCastTargets();
+			
+
+		if (isTransparent && color.a != 0) {
+			color.a -= Time.deltaTime * speed;
+			transparent.color = color;
+			if (color.a <= 0) {
+				color.a = 0;
+			}
+
+		} else if (!isTransparent && color.a != 1) {
+			color.a += Time.deltaTime * speed;
+			transparent.color = color ;
+
+
+			if (color.a >= 1) {
+				color.a =1;
+				originalMat.color = color;
+				rend.material = originalMat;
+			}
+
+		}
+
+	}
+
+	void rayCastTargets(){
+
+
+		for (int i = 0; i < targetObjects.Length ; i++) {
+
+			RaycastHit hitPoint;
+	
+
+			if (Physics.Raycast (Camera.main.transform.position, targetObjects[i].transform.position - Camera.main.transform.position, out hitPoint)) {
+				
+				if (hitPoint.transform == transform) {
+					
+					makeTransparent ();
+				} else {
+					makeSolid ();
+				}
+
+			}
+
+		}
+	}
+
+
+
+	void rayCast(){
+		
+		RaycastHit hitPoint;
+        
+	
+		if (Physics.Raycast (Camera.main.transform.position, player.transform.position - Camera.main.transform.position, out hitPoint)) {
+
+			if (hitPoint.transform == transform) {
+								
+				makeTransparent ();
+			} else {
+				makeSolid ();
+			}
+
+		}
+
+	
+  }
+
+}
