6517214c2bd66a8b1c9e8c8f79dd28e762c46cf6
umr <umurcg@gmail.com>
2016-10-09 01:11:25 +0300

    Hitchcock shot

---
+++ The Overcoat/Assets/Scripts/ObjectController/MouseDrag.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseDrag : MonoBehaviour
+{
+
+    bool touched = false;
+    public float speed = 10f;
+
+    //public Vector3 offset ;
+
+
+    //for child scripts
+
+
+    // Use this for initialization
+    protected void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X")) * Time.deltaTime * speed;
+
+    }
+
+
+
+
+    public virtual void dragObject()
+    {
+
+        //transform.position += new Vector3 (Input.GetAxis ("Mouse X"), Input.GetAxis ("Mouse Y"), 0) * Time.deltaTime * speed;
+
+        transform.position = Vector3.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), Time.deltaTime * speed);
+
+        //		if (Input.GetAxis ("Mouse X") !=0|| Input.GetAxis ("Mouse Y")!=0) {
+        //			transform.position += (transform.up/3-transform.forward)* Time.deltaTime*speed;
+        //		}
+    }
+
+}
---
+++ The Overcoat/Assets/getLost.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getLost : MonoBehaviour {
+
+	// Use this for initialization
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.SetActive(false);
+    }
+}
