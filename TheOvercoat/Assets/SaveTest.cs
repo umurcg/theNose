bcf15725d23f5eee29a761e4c8372a2664c320f4
Umur Can Gurelli <umurcg@gmail.com>
2017-06-15 16:36:05 +0300

    again recover

---
+++ TheOvercoat/Assets/SaveTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveTest : GameController, IClickAction{
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public override void Action()
+    {
+        if (isUsed())
+        {
+            sc.callSubtitleWithIndex(0);
+
+        }else
+        {
+            sc.callSubtitleWithIndex(1);
+        }
+    }
+
+}
