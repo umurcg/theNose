85270000e277f295acb9d36aed3346a41056cb23
umurcg <umurcg@gmail.com>
2017-08-16 14:54:55 +0300

    language prompt

---
+++ TheOvercoat/Assets/MenuLanguagePrompt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLanguagePrompt : MonoBehaviour {
+
+    public GameObject mainMenu;
+
+
+    public void turkish()
+    {
+        GlobalController.Instance.setLanguageSetting((int)GlobalController.Language.TR);
+        mainMenu.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void english()
+    {
+        GlobalController.Instance.setLanguageSetting((int)GlobalController.Language.ENG);
+        mainMenu.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
